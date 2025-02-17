31f0be44ef146d5b61358eb32683e518ba766721
felipe alves <131407431+fefe6777@users.noreply.github.com>
2025-02-17 16:33:08 -0300

    Add files via upload

--- mysql/sql.sql
+++ mysql/sql.sql
@@ -2,7 +2,15 @@ USE Empresa;
 
 CREATE TABLE funcionarios(
     id int AUTO_INCREMENT PRIMARY KEY,
-    funcionario VARCHAR(60) NOT NULL,
-    cidade VARCHAR (50) NOT NULL,
-    data_contratacao DATE NOT NULL
+    cliente VARCHAR(60) NOT NULL,
+    endereco VARCHAR (50) NOT NULL,
+    telefone VARCHAR (50) NOT NULL,
+    cep VARCHAR (15) NOT NULL
+)
+
+CREATE TABLE produtos(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    produto VARCHAR(60) NOT NULL,
+    estoque VARCHAR (50) NOT NULL,
+    preco VARCHAR (50) NOT NULL
 )
